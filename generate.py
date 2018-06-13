027089cc7eb2adf76503b48619dd6a4f002b0706
_rika <i@ksqsf.moe>
2018-06-13 19:45:04 +0800

    fix a bug

--- generate.py
+++ generate.py
@@ -8,10 +8,11 @@ pat = re.compile(DIALOG_PATTERN)
 with open('script-ch30.rpy') as f:
   in_dialog = False
   for line in f:
+    if line[0] == '#': continue
     mat = pat.search(line)
     if mat:
       print(mat.group())
       in_dialog = True
-    elif in_dialog:
+    elif in_dialog and line[0] != ' ':
       print('%')
       in_dialog = False
